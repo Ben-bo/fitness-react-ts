358ad844190a6013bec39f39c628fe4f3379c466
Beni Iskandar <bennyizkandar@gmail.com>
2023-03-13 09:23:05 +0800

    add benefit komponene

---
+++ src/components/BenefitsComponent/index.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import { SelectedPage } from "../shared/enumPage";
+import Htext from "../shared/Htext";
+import { benefitsDataType } from "../shared/typeAndInterface";
+import BenefitsCardComponent from "../shared/benefitsCard";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+const dataBenefits: Array<benefitsDataType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of The Art Facilities",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex debitis alias corrupti hic incidunt at officiis, nobis qui neque molestiae, nisi sunt nemo quaerat suscipit quasi! Laborum animi labore velit?",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Divers Classes",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex debitis alias corrupti hic incidunt at officiis, nobis qui neque molestiae, nisi sunt nemo quaerat suscipit quasi! Laborum animi labore velit?",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex debitis alias corrupti hic incidunt at officiis, nobis qui neque molestiae, nisi sunt nemo quaerat suscipit quasi! Laborum animi labore velit?",
+  },
+];
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChilder: 0.2 },
+  },
+};
+
+const BenefitsComponent = ({ setSelectedPage }: Props) => {
+  return (
+    <section
+      id={SelectedPage.Benefits}
+      className="mx-auto min-h-full w-5/6 py-20 md:mt-32"
+    >
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        <div className="md:my-5 md:w-3/5">
+          <Htext>MORE THAN JUST A GYM</Htext>
+          <p className="my-5 text-sm">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
+            similique architecto officia minus delectus, facere incidunt quos ut
+            minima molestiae temporibus natus impedit magni id repellendus a
+            labore et. Minima?
+          </p>
+        </div>
+
+        {/* benefits card */}
+        <motion.div
+          className=" mt-5 md:flex items-center justify-between gap-8 "
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {dataBenefits.map((value: benefitsDataType) => (
+            <BenefitsCardComponent
+              key={value.title}
+              icon={value.icon}
+              title={value.title}
+              description={value.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default BenefitsComponent;
