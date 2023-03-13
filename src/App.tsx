be70d6b8387937ec6dcda7cd39c80a844428f52b
Beni Iskandar <bennyizkandar@gmail.com>
2023-03-13 09:26:16 +0800

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { SelectedPage } from "./components/shared/enumPage";
 import NavbarComponent from "./components/NavbarComponent";
 import HomeComponent from "./components/HomeComponent";
+import BenefitsComponent from "./components/BenefitsComponent";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -38,6 +39,7 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <HomeComponent setSelectedPage={setSelectedPage} />
+      <BenefitsComponent setSelectedPage={setSelectedPage} />
     </div>
   );
 }
