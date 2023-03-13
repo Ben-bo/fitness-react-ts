8b72e6917b54cccc24b15e83edf1dcb9741b64a6
Beni Iskandar <bennyizkandar@gmail.com>
2023-03-13 09:56:28 +0800

    add github pages depedency

--- src/components/BenefitsComponent/index.tsx
+++ src/components/BenefitsComponent/index.tsx
@@ -50,7 +50,17 @@ const BenefitsComponent = ({ setSelectedPage }: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div className="md:my-5 md:w-3/5">
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           <Htext>MORE THAN JUST A GYM</Htext>
           <p className="my-5 text-sm">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
@@ -58,7 +68,7 @@ const BenefitsComponent = ({ setSelectedPage }: Props) => {
             minima molestiae temporibus natus impedit magni id repellendus a
             labore et. Minima?
           </p>
-        </div>
+        </motion.div>
 
         {/* benefits card */}
         <motion.div
