a907a2da44bd2c89a9386f78d2c4cae01e723f5a
Beni Iskandar <bennyizkandar@gmail.com>
2023-03-14 09:55:00 +0800

    update benfits component

--- src/components/BenefitsComponent/index.tsx
+++ src/components/BenefitsComponent/index.tsx
@@ -9,6 +9,8 @@ import { SelectedPage } from "../shared/enumPage";
 import Htext from "../shared/Htext";
 import { benefitsDataType } from "../shared/typeAndInterface";
 import BenefitsCardComponent from "../shared/benefitsCard";
+import ActionButton from "../shared/actionButton";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -88,6 +90,90 @@ const BenefitsComponent = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
+
+        {/* graphic and description */}
+
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          {/* image */}
+          <img
+            src={BenefitsPageGraphic}
+            alt="benfits-image"
+            className="mx-auto"
+          />
+          {/* description */}
+          <div>
+            {/* title */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves ">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <Htext>
+                    MILIION OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>
+                  </Htext>
+                </motion.div>
+              </div>
+            </div>
+
+            {/* main descrip */}
+
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className="my-5">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
+                iure dolor natus aliquam eligendi iste molestias quae fugit
+                omnis, eveniet illo! Aut, dignissimos optio. Totam rem fugit
+                magnam minus numquam!
+              </p>
+              <p className="mb-5">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Maiores, alias maxime ratione tenetur accusantium ea libero aut
+                nam ipsam corrupti quis earum voluptatum numquam laboriosam
+                atque. Labore, ad consectetur! Voluptatem?
+              </p>
+            </motion.div>
+
+            {/* button */}
+
+            <div className="relative mt-16 ">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ delay: 0.5, duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <ActionButton
+                    setSelectedPage={setSelectedPage}
+                    pageName={SelectedPage.Benefits}
+                  >
+                    Join Now
+                  </ActionButton>
+                </motion.div>
+              </div>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
